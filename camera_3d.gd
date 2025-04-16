f182aa40ea7df61bc8b82a4683d05e2944fc369b
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2025-04-16 21:13:20 +0300

    better movement, collision, lightning

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,4 +1,4 @@
-extends Camera3D
+extends CharacterBody3D
 
 @export var movement_step: float = 1
 @export var rotation_step: float = 10
@@ -10,13 +10,13 @@ func _physics_process(delta):
 		get_tree().quit()
 	
 	var movement: Movement = Movement.None
-	if Input.is_key_pressed(KEY_UP):
+	if Input.is_action_just_pressed("move_forward"):
 		movement = Movement.Forward
-	if Input.is_key_pressed(KEY_DOWN):
+	if Input.is_action_just_pressed("move_backward"):
 		movement = Movement.Backward
-	if Input.is_key_pressed(KEY_LEFT):
+	if Input.is_action_just_pressed("move_left"):
 		movement = Movement.Left
-	if Input.is_key_pressed(KEY_RIGHT):
+	if Input.is_action_just_pressed("move_right"):
 		movement = Movement.Right
 	
 	if movement == Movement.None:
@@ -25,13 +25,15 @@ func _physics_process(delta):
 	match movement:
 		Movement.Forward:
 			var step: Vector3 = -self.transform.basis.z
-			self.transform.origin += step
+			#self.transform.origin += step
+			move_and_collide(step)
 		Movement.Backward:
-			var step: Vector3 = -self.transform.basis.z
-			self.transform.origin -= step
+			var step: Vector3 = self.transform.basis.z
+			#self.transform.origin -= step
+			move_and_collide(step)
 		Movement.Left:
 			self.rotate_y(deg_to_rad(rotation_step))
 		Movement.Right:
 			self.rotate_y(deg_to_rad(-rotation_step))
 	
-	self.transform.origin.y = 2
+	#self.transform.origin.y = 2
